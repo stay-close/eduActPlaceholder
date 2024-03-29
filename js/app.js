636acc46285bf0494dbcbbc923a06f9007ffbaaa
stay-close <swapansapu2@gmail.com>
2024-03-29 23:05:16 +0600

    collapse menu code done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 // collapse menu
 
 const collapseMenuBar=document.querySelector('.collapse_menu_bar');
+const collapseMenuBarIcon=document.querySelector('.collapse_menu_bar i');
 const mainMenu=document.querySelector('.main_menu');
 
 collapseMenuBar.addEventListener('click',(e)=>{
 mainMenu.classList.toggle('active');
+collapseMenuBarIcon.classList.toggle('fa-xmark');
+
+
 })
